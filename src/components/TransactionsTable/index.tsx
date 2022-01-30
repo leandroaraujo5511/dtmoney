8ceb815c0fb121122491b5cf6024377c37dbd39b
Leandro Araujo <leandroaraujo5511@gmail.com>
2022-01-29 22:16:40 -0300

    Configurando MirageJs + Asios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable(){
+    
+    useEffect(()=>{
+        api.get('transactions')
+            .then(reponse => console.log(reponse.data))
+    },[])
+    
+    
+    
     return(
         <Container>
             <table>
